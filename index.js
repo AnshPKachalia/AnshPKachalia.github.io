566e35ec14b63898ed404a4c156cba2c985e858a
AnshPKachalia <71828670+AnshPKachalia@users.noreply.github.com>
2020-09-29 19:49:58 +0400

    zoom image while hover

---
+++ index.js
@@ -0,0 +1,89 @@
+//Zoom on hover image 1
+document.getElementById("fire").addEventListener('mouseover',function(){
+    document.getElementById("fire").style.transform="scale(1.05)";
+});
+
+document.getElementById("fire").addEventListener("mouseout", function() {
+    document.getElementById("fire").style.transform="scale(1)";
+});
+
+//Zoom on hover image 2
+document.getElementById("trump").addEventListener('mouseover',function(){
+    document.getElementById("trump").style.transform="scale(1.05)";
+});
+
+document.getElementById("trump").addEventListener("mouseout", function() {
+    document.getElementById("trump").style.transform="scale(1)";
+});
+
+//Zoom on hover image 3
+document.getElementById("olympics").addEventListener('mouseover',function(){
+    document.getElementById("olympics").style.transform="scale(1.05)";
+});
+
+document.getElementById("olympics").addEventListener("mouseout", function() {
+    document.getElementById("olympics").style.transform="scale(1)";
+});
+
+//Zoom on hover image 4
+document.getElementById("royal").addEventListener('mouseover',function(){
+    document.getElementById("royal").style.transform="scale(1.05)";
+});
+
+document.getElementById("royal").addEventListener("mouseout", function() {
+    document.getElementById("royal").style.transform="scale(1)";
+});
+
+//Zoom on hover image 5
+document.getElementById("stock").addEventListener('mouseover',function(){
+    document.getElementById("stock").style.transform="scale(1.05)";
+});
+
+document.getElementById("stock").addEventListener("mouseout", function() {
+    document.getElementById("stock").style.transform="scale(1)";
+});
+
+//Zoom on hover image 6
+document.getElementById("george").addEventListener('mouseover',function(){
+    document.getElementById("george").style.transform="scale(1.05)";
+});
+
+document.getElementById("george").addEventListener("mouseout", function() {
+    document.getElementById("george").style.transform="scale(1)";
+});
+
+//Zoom on hover image 7
+document.getElementById("twitter").addEventListener('mouseover',function(){
+    document.getElementById("twitter").style.transform="scale(1.05)";
+});
+
+document.getElementById("twitter").addEventListener("mouseout", function() {
+    document.getElementById("twitter").style.transform="scale(1)";
+});
+
+//Zoom on hover image 8
+document.getElementById("nisarga").addEventListener('mouseover',function(){
+    document.getElementById("nisarga").style.transform="scale(1.05)";
+});
+
+document.getElementById("nisarga").addEventListener("mouseout", function() {
+    document.getElementById("nisarga").style.transform="scale(1)";
+});
+
+//Zoom on hover image 9
+document.getElementById("usiran").addEventListener('mouseover',function(){
+    document.getElementById("usiran").style.transform="scale(1.05)";
+});
+
+document.getElementById("usiran").addEventListener("mouseout", function() {
+    document.getElementById("usiran").style.transform="scale(1)";
+});
+
+//Zoom on hover image 10
+document.getElementById("harvey").addEventListener('mouseover',function(){
+    document.getElementById("harvey").style.transform="scale(1.05)";
+});
+
+document.getElementById("harvey").addEventListener("mouseout", function() {
+    document.getElementById("harvey").style.transform="scale(1)";
+});
